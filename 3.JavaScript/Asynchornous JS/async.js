ea0d8e6442c7a9509fb809c2c834b62fb74686df
Suraj <kumarsuraj54057@gmail.com>
2022-09-05 18:43:02 +0530

    Asyncronous Js

---
+++ 3.JavaScript/Asynchornous JS/async.js
@@ -0,0 +1,14 @@
+
+console.log("Task 1");
+
+setTimeout(() => {
+    console.log("Task 2")
+}, 5000);
+
+setTimeout(() => {
+    console.log("Task 2.5")
+}, 1000);
+
+console.log("Task 3");
+
+console.log("Task 4");
