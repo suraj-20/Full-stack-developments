0abd7abf58314fe16dde1b718257c43338a1890c
Suraj <kumarsuraj54057@gmail.com>
2022-09-05 18:41:30 +0530

    Variables in Js

---
+++ 3.JavaScript/variables.js
@@ -0,0 +1,76 @@
+
+// What are variables ? --> Variables are containers for storing data (storing data values).
+
+// How to declare a variables in JS ?
+
+// var means variable.
+
+var num = 10;
+num = 30;
+console.log(num);
+
+// How to declare a const in JS ?
+
+const marks = 100;
+// marks = 200;   (we did not reinisialize the value in const.)
+console.log(marks);
+
+// How to declare a let in JS ?
+
+var myName = "Suraj"
+myName = "Kumar"
+console.log(myName);
+
+// Declare x, y, z using var keywords.
+
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+// Declare a, b, c using let keywords.
+
+
+let a = 5; 
+let b = 6;
+let c = a + b;
+console.log(z);
+
+// Declare d, e, f without using any keywords.
+
+d = 5;
+e = 6;
+f = d + e;
+console.log(f);
+
+const pi = 3.14;
+let person = 'John Doe';
+let answer = 'Yes I am!';
+console.log(pi + " " + person + " " + answer);
+
+// Declare many variables in one line.
+
+let name = 'suraj', carName = 'Thar', price = 130;
+console.log(price);
+
+// Re-declare a JavaScript variable declared with var.
+
+var CarName = "Thar";
+var CarName;
+console.log(CarName);
+
+// Cannot re-declare a variable declared with let or const.
+
+// let CarName = "Thar";
+// let CarName;
+// console.log(CarName);
+
+// Addition of values.
+
+let xy = 2 + 5 + 6;
+console.log(xy);
+
+// Concatination of two strings.
+
+let Name = 'Suraj' + " " + 'Kumar';
+console.log(Name);
