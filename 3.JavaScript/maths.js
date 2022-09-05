c8b8f4a59f107e78461af15dd46115246dd77277
Suraj <kumarsuraj54057@gmail.com>
2022-09-05 18:39:40 +0530

    Maths in Js

---
+++ 3.JavaScript/maths.js
@@ -0,0 +1,28 @@
+
+// Maths object in JavaScript.
+
+// 1. How to round a decimal value.
+// Math.round returns nearest integer.
+console.log(Math.round(4.6));  // 5
+console.log(Math.round(4.4));  // 4
+
+// 2. How to get nearest integer rounded up.
+console.log(Math.ceil(4.2));  // 5
+
+// 3. How to get nearest integer rounded down.
+console.log(Math.floor(4.9));  // 4
+
+// 4. How to get square root of a number.
+console.log(Math.sqrt(64));  // 8
+
+// 5. How to get power of a number.
+console.log(Math.pow(8, 2));  // 64
+
+// 6. How to get maximum value from a collection of values.
+console.log(Math.max(10, 25, 30, 65, 91, 55));  // 91
+
+// 7. How to get minimum value from a collection of values.
+console.log(Math.min(10, 25, 30, 65, 91, 55))  // 10
+
+// 8. Get any rendom number between 0 and 1.
+console.log(Math.random());
